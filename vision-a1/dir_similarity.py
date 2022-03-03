537bda4d9efda8464a52bbb35f65b915f14c3550
rysias <jhvithamar@gmail.com>
2022-03-03 15:00:03 +0100

    make multiprocessing work!

--- vision-a1/dir_similarity.py
+++ vision-a1/dir_similarity.py
@@ -15,11 +15,27 @@ def find_similarity(pair_tuble: Tuple[Tuple[Path, np.ndarray], Tuple[Path, np.nd
     pair2 = pair_tuble[1]
     return {"key": create_key(pair1[0], pair2[0]), "dist": fsi.compare_hists(pair1[1], pair2[1])}
 
-def find_all_similarities(hist_dict: List[Path], n_cores: int) -> List[Dict]: 
+def find_all_similarities(hist_dict: List[Path], n_cores: int) -> pd.DataFrame: 
     all_hist_pairs = itertools.combinations(hist_dict.items(), 2)
     with Pool(n_cores) as p:
         master_list = p.map(find_similarity, all_hist_pairs)
-    return master_list
+    return pd.DataFrame(master_list)
+
+def create_dist_row(arg: Tuple[Path, pd.DataFrame]) -> pd.DataFrame:
+    source_path, dist_df = arg
+    col_filter = dist_df["key"].str.contains(source_path.name)
+    smallest_dists = dist_df[col_filter].nsmallest(3, "dist")
+    smallest_dists["target"] = smallest_dists["key"].str.replace(source_path.name, "").str.replace("-", "")
+    smallest_dists["source"] = source_path.name
+    smallest_dists["rank"] = ["1st", "2nd", "3rd"]    
+    return smallest_dists.pivot(index="source", columns = "rank", values="target")
+
+
+def create_closest_df(img_paths: List[Path], dist_df: pd.DataFrame, n_cores: int) -> pd.DataFrame:
+    dist_input = ((img_path, dist_df) for img_path in img_paths)
+    with Pool(n_cores) as p:
+        df_list = p.map(create_dist_row, dist_input)
+    return pd.concat(df_list)
 
 def main(args):
     DATA_DIR = Path(args.data_dir)
@@ -31,8 +47,10 @@ def main(args):
     # Heavy lifting y'all!
     master_dict = fsi.create_master_hists(all_img_paths, n_cores=ncores)
     all_dists = find_all_similarities(master_dict, ncores)
-    print(all_dists[:5])
     
+    # Find closest for all 
+    closest_df = create_closest_df(all_img_paths, all_dists, ncores)
+    closest_df.to_csv(OUTPUT_DIR / f"{DATA_DIR.parent.name}_dists.csv")
     
 
 if __name__ == "__main__":
