060e41de585e173677ad65488642f55b47245e10
Rysias <jhvithamar@gmail.com>
2022-03-21 14:51:27 +0100

    create full running script

---
+++ language-a3/run_project.sh
@@ -0,0 +1,8 @@
+# Script for running everything :))
+echo "installing dependencies"
+pipenv install --dev
+echo "running tests"
+pipenv run python -m pytest
+echo "Running the script!..."
+pipenv run python network_analysis.py --data-path "../../../CDS-LANG/network_data"
+
