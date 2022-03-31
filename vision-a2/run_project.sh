b5a31dde3843ef716feb23163cdd325fdc34d943
rysias <jvhithamar@gmail.com>
2022-03-31 12:46:10 +0100

    all done?

--- vision-a2/run_project.sh
+++ vision-a2/run_project.sh
@@ -2,6 +2,10 @@
 echo "installing dependencies"
 pip install pipenv
 pipenv install
-echo "Running the example script!..."
-pipenv run python network_analysis.py --data-path "../../../CDS-LANG/network_data"
+echo "Running the scripts!..."
+echo "Run logistic regression"
+pipenv run python logistic_regression.py --dataset "cifar10"
+echo "Run neural network"
+pipenv run python nn_classifier.py --epochs 3 --dataset "mnist"
+echo "All done!"
 
