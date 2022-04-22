c3f98aebd6f8cd81abb0c870f2a1ff39e5139e76
rysias <jvhithamar@gmail.com>
2022-04-22 11:28:18 +0200

    update script

--- vision-a3/run_project.sh
+++ vision-a3/run_project.sh
@@ -2,10 +2,7 @@
 echo "installing dependencies"
 pip install pipenv
 pipenv install
-echo "Running the scripts!..."
-echo "Run logistic regression"
-pipenv run python logistic_regression.py --dataset "cifar10"
-echo "Run neural network"
-pipenv run python nn_classifier.py --epochs 3 --dataset "mnist"
+echo "Running main script!"
+pipenv run python transfer_cnn.py
 echo "All done!"
 
