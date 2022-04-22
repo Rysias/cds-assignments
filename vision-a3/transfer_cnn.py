27f87bbafb18264ad5c5a43c8ce0a6a5c02ede22
rysias <jvhithamar@gmail.com>
2022-04-22 09:56:45 +0200

    add a bit

--- vision-a3/transfer_cnn.py
+++ vision-a3/transfer_cnn.py
@@ -6,8 +6,20 @@
 - Save the classification report
 """
 import src.load_data as load_data
+import tensorflow as tf
 
 
 def main():
-    # Load the CIFAR10 dataset
+    # Load the CIFAR10 dataset (NB: Make sure input size is right!)
     x_train, y_train, x_test, y_test = load_data.load_cifar10()
+
+    # Load the VGG16 model
+    base_model = tf.keras.applications.vgg16.VGG16(
+        include_top=False, weights="imagenet", classifier_activation="softmax",
+    )
+
+    base_model.trainable = False
+
+
+if __name__ == "__main__":
+    main()
