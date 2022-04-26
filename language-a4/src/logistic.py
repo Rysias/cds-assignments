69bcef8de670114e7271c5f1dd3aa22d241b80fa
rysias <jvhithamar@gmail.com>
2022-04-26 09:32:32 +0200

    add logistic

---
+++ language-a4/src/logistic.py
@@ -0,0 +1,28 @@
+from imblearn.pipeline import Pipeline
+from imblearn.under_sampling import RandomUnderSampler
+
+# import columntransformer from sklearn
+from sklearn.compose import ColumnTransformer
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def create_pipeline(textcol: str = "clean_text") -> Pipeline:
+    vectorizer = TfidfVectorizer(stop_words="english")
+    sampler = RandomUnderSampler(random_state=1)
+    logistic = LogisticRegression()
+    return Pipeline(
+        [
+            ("resample", sampler),
+            (
+                "column_transformer",
+                ColumnTransformer(
+                    [("comment_text_vectorizer", vectorizer, textcol)],
+                    remainder="passthrough",
+                ),
+            ),
+            ("model", logistic),
+        ]
+    )
+
