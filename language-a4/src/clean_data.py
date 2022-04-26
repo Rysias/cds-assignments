b28825bb53d2d0081b38d8264f6fb9cfb8862e1a
rysias <jvhithamar@gmail.com>
2022-04-26 09:31:03 +0200

    add clean_data file

---
+++ language-a4/src/clean_data.py
@@ -0,0 +1,41 @@
+"""
+Cleans textual data for prediction tasks
+"""
+import pandas as pd
+from pathlib import Path
+
+
+def clean_text(text: pd.Series) -> pd.Series:
+    """ Remove whitespace and punctuation """
+    return (
+        text.str.replace(r"\s+", " ", regex=True)
+        .str.replace(r"[^\w\s]", "", regex=True)
+        .str.rstrip()
+    )
+
+
+def remove_short_text(
+    df: pd.DataFrame, textcol: str = "text", min_length: int = 10
+) -> pd.DataFrame:
+    """
+    Remove text that is too short
+    """
+    text_filter = df[textcol].str.len() >= min_length
+    return df[text_filter]
+
+
+def clean_df(df: pd.DataFrame, textcol: str = "text") -> pd.DataFrame:
+    """
+    Cleans dataframe with text column
+    """
+    clean_df = df.copy()
+    clean_df[textcol] = clean_text(df[textcol])
+    return remove_short_text(clean_df, textcol)
+
+
+def read_and_clean(filename: str) -> pd.DataFrame:
+    """
+    Reads and cleans dataframe from csv file
+    """
+    df = pd.read_csv(filename)
+    return clean_df(df)
