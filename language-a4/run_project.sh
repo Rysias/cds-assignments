fd9f190d09a3bb45cde84aecc335b26f03a1d457
Rysias <jhvithamar@gmail.com>
2022-04-26 14:59:38 +0200

    update run

--- language-a4/run_project.sh
+++ language-a4/run_project.sh
@@ -6,8 +6,8 @@ echo "running tests"
 pipenv run python -m pytest
 echo "Running the scripts..."
 echo "Run logistic regression..."
-pipenv run python logistic_regression.py
+pipenv run python logistic_regression.py --dataset "../../CDS-VIS/toxic/VideoCommentsThreatCorpus.csv"
 echo "Done! Running neural network..."
-pipenv run python dnn_text.py --epochs 50
+pipenv run python dnn_text.py --dataset "../../CDS-VIS/toxic/VideoCommentsThreatCorpus.csv" --epochs 50
 echo "Done with everything!"
 
