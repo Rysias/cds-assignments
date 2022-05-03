486e6196bda2d4d83f5b751d143c77f74e5ee8d3
Rysias <jhvithamar@gmail.com>
2022-05-03 15:07:25 +0200

    more stuff

--- language-a1/run_project.sh
+++ language-a1/run_project.sh
@@ -2,7 +2,11 @@
 echo "setting up..."
 bash setup.sh
 echo "Running the scripts..."
-echo "collocate single text"
-pipenv run python collocate_single_text.py --file-name "Dickens_Bleak_1853.txt" --search-term "bleak"
+echo "Collocating 'bleak' in 'Bleak'..."
+python collocate_single_text.py --file-name "Dickens_Bleak_1853.txt" --search-term "bleak"
+echo "collocate all texts for love..."
+python collocate_all_texts.py --window-size 3 --search-term "love"
+echo "finding both love and hate in Dickens..."
+python collocate_multi_words.py --search-terms love hate --window-size 3
 echo "Done with everything!"
 
