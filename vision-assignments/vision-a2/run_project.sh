33c9cdd02547b2f0a45c39b288870cea9d093886
Rysias <jhvithamar@gmail.com>
2022-05-15 16:54:06 +0200

    update everything

--- vision-assignments/vision-a2/run_project.sh
+++ vision-assignments/vision-a2/run_project.sh
@@ -1,11 +1,11 @@
 # Script for running everything :))
-echo "installing dependencies"
+echo 'installing dependencies'
 pip install pipenv
-pipenv install
+python -m pipenv install
 echo "Running the scripts!..."
 echo "Run logistic regression"
-pipenv run python logistic_regression.py --dataset "mnist"
+pipenv run python logistic_regression.py --dataset "cifar10"
 echo "Run neural network"
-pipenv run python nn_classifier.py --epochs 200 --dataset "mnist"
+pipenv run python nn_classifier.py --epochs 3 --dataset "cifar10"
 echo "All done!"
 
