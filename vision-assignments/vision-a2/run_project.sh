88c69a989160413da308fe74da1b381c797b5d8e
Rysias <jhvithamar@gmail.com>
2022-05-15 12:03:03 +0200

    update pipfile

--- vision-assignments/vision-a2/run_project.sh
+++ vision-assignments/vision-a2/run_project.sh
@@ -6,6 +6,6 @@ echo "Running the scripts!..."
 echo "Run logistic regression"
 pipenv run python logistic_regression.py --dataset "mnist"
 echo "Run neural network"
-pipenv run python nn_classifier.py --epochs 3 --dataset "mnist"
+pipenv run python nn_classifier.py --epochs 200 --dataset "mnist"
 echo "All done!"
 
