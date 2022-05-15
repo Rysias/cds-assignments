7ba9aaebc4b1b8ada6c74777c74be5c7d1ea00d0
Rysias <jhvithamar@gmail.com>
2022-05-15 11:51:10 +0200

    update data download

--- vision-assignments/vision-a1/run_project.sh
+++ vision-assignments/vision-a1/run_project.sh
@@ -2,6 +2,8 @@
 echo "installing dependencies"
 pip install pipenv
 pipenv install
+echo "Download data"
+python download_data.py
 echo "Running the scripts!..."
 echo "Run single image similarity"
 pipenv run python find_similar_imgs.py --img-name "image_0003.jpg"
