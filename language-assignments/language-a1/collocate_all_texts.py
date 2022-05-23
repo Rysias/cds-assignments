d834c643dc22a3d840fdb7ba6ded53f26dd52519
Rysias <jhvithamar@gmail.com>
2022-05-23 16:12:38 +0200

    add logging and fix script

--- language-assignments/language-a1/collocate_all_texts.py
+++ language-assignments/language-a1/collocate_all_texts.py
@@ -3,7 +3,12 @@
 """
 import src.collocate as clt
 import argparse
+import logging
 from pathlib import Path
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 
 def tokenize_docs(texts, n_cores=1):
@@ -11,15 +16,18 @@ def tokenize_docs(texts, n_cores=1):
 
 
 def main(args):
+    RUN_TEST = args.test_mode
     search_term = args.search_term
     window_size = args.window_size
     path_dir = Path(args.corpus_path)
 
+    logging.info("Cleaning all texts...")
     all_texts = [
         (clt.clean_file(file), file)
         for i, file in enumerate(path_dir.glob("*.txt"))
         if i < 11
     ]
+    logging.info("Tokenizing all texts...")
     all_docs = tokenize_docs(text for text, _ in all_texts)
     files = [file for _, file in all_texts]
     for i, text in enumerate(all_docs):
@@ -27,15 +35,18 @@ def main(args):
         collate_df = clt.collocate_pipeline(corpus, search_term, window_size)
         clt.write_output(collate_df, files[i].name, search_term)
         if i % 10 == 0:
-            print(f"processed {i+1} of {len(all_texts)} files")
+            logging.info(f"processed {i+1} of {len(all_texts)} files")
 
-        if i == 10:
+        if i == 10 and RUN_TEST:
             break
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(
         description="Creates collocation info for all files in corpus. The output is written to a csv with the name '{text_name}_{search_string}.csv' in the output folder"
+    )
+    argparser.add_argument(
+        "--test-mode", action="store_true", help="Runs a small test", default=False
     )
     argparser.add_argument(
         "--corpus-path",
--- language-assignments/language-a1/run_project.sh
+++ language-assignments/language-a1/run_project.sh
@@ -7,8 +7,8 @@ echo "Running the scripts..."
 echo "Collocating 'bleak' in 'Bleak'..."
 python collocate_single_text.py --file-name "Dickens_Bleak_1853.txt" --search-term "bleak"
 echo "collocate all texts for love..."
-python collocate_all_texts.py --window-size 3 --search-term "love"
-echo "finding both love and hate in Dickens..."
-python collocate_multi_words.py --search-terms love hate --window-size 3
+python collocate_all_texts.py --window-size 3 --search-term "love" --test-mode
+echo "finding both love and hate in 'Oliver Twist'..."
+python collocate_multi_words.py --file-name "Dickens_Oliver_1839.txt" --search-terms love hate --window-size 3
 echo "Done with everything!"
 
