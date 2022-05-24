74f3a779429b5eabd624a59a01cb11d144fcd30a
Rysias <jhvithamar@gmail.com>
2022-05-24 13:55:22 +0200

    all output a3

--- language-assignments/language-a3/run_project.sh
+++ language-assignments/language-a3/run_project.sh
@@ -2,6 +2,7 @@
 echo "installing dependencies"
 pip install pipenv
 pipenv install --dev
+pipenv run sudo apt-get install wkhtmltopdf
 echo "running tests"
 pipenv run python -m pytest
 echo "Running the script!..."
