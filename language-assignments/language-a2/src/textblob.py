7cc2152ad4b4d15dd4bfb37b7f09e6b4a36663d2
Rysias <jhvithamar@gmail.com>
2022-05-24 11:23:12 +0200

    create sentiments

---
+++ language-assignments/language-a2/src/textblob.py
@@ -0,0 +1,12 @@
+import spacy  # type: ignore
+from spacy import Doc  # type: ignore
+from typing import Callable
+
+
+def textblob_sentiment(doc: Doc) -> float:
+    return doc._.blob.polarity
+
+
+def initialise_textblob(spacy_model: spacy.lang) -> Callable[[Doc], float]:
+    spacy_model.add_pipe("spacytextblob")
+    return textblob_sentiment
---
+++ language-assignments/language-a2/src/vader.py
@@ -0,0 +1,13 @@
+import functools
+from spacy import Doc  # type: ignore
+from typing import Callable
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+def vader_sentiment(doc: Doc, analyzer: SentimentIntensityAnalyzer) -> float:
+    return analyzer.polarity_scores(doc.text)["compound"]
+
+
+def initialise_vader() -> Callable[[Doc], float]:
+    analyzer = SentimentIntensityAnalyzer()
+    return functools.partial(vader_sentiment, analyzer=analyzer)
