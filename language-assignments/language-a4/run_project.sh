f7703b314f9d5e0a52f7d9e6a9275109f3da8338
Rysias <jhvithamar@gmail.com>
2022-05-25 11:10:12 +0200

    update setup

--- language-assignments/language-a4/run_project.sh
+++ language-assignments/language-a4/run_project.sh
@@ -1,11 +1,10 @@
 # Script for running everything :))
 echo "installing dependencies"
-pip install pipenv
-pipenv install --dev
+bash setup.sh
 echo "Running the scripts..."
 echo "Run logistic regression..."
 pipenv run python logistic_regression.py --dataset "../../../CDS-LANG/toxic/VideoCommentsThreatCorpus.csv"
 echo "Done! Running neural network..."
-pipenv run python dnn_text.py --dataset "../../../CDS-LANG/toxic/VideoCommentsThreatCorpus.csv" --epochs 50
+pipenv run python dnn_text.py --dataset "input/VideoCommentsThreatCorpus.csv" --epochs 5
 echo "Done with everything!"
 
