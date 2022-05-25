d17139f036e4b66e7242bad391a4dba127266a11
Rysias <jhvithamar@gmail.com>
2022-05-25 16:35:11 +0200

    a lot of pain

--- language-assignments/language-a4/clean_toxic.py
+++ language-assignments/language-a4/clean_toxic.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 if __name__ == "__main__":
     rawdf = pd.read_csv("input/train.csv")
-    df = rawdf[rawdf["toxic"] == 1, ["toxic", "comment_text"]]
+    df = rawdf.loc[rawdf["toxic"] == 1, ["toxic", "comment_text"]]
     df.columns = ["label", "text"]
     df.to_csv("input/toxic_train.csv", index=False)
