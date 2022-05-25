e5f478e0d875c683429235d274c70b0e5aa65ba5
Rysias <jhvithamar@gmail.com>
2022-05-25 13:03:38 +0200

    create aug

---
+++ language-assignments/language-a4/src/augment.py
@@ -0,0 +1,40 @@
+import logging
+import nlpaug.augmenter.word as naw  # type: ignore
+import nltk
+import pandas as pd
+import numpy as np
+from typing import List
+
+aug = naw.SynonymAug(aug_src="wordnet", aug_max=2)
+
+
+def handle_lookup(e: Exception, missing_import: str):
+    if missing_import in str(e):
+        nltk.download(missing_import)
+        return True
+
+
+def repeat_df(df: pd.DataFrame, n=1):
+    return pd.DataFrame(np.repeat(df.to_numpy(), n, axis=0), columns=df.columns)
+
+
+def handle_lookups(e: Exception):
+    imports = ["wordnet", "averaged_perceptron_tagger", "omw-1.4"]
+    for missing_import in imports:
+        if handle_lookup(e, missing_import):
+            return True
+    raise e
+
+
+def synonym_augment(
+    df: pd.DataFrame, text_col: str = "text", label_col: str = "label", n: int = 2
+) -> pd.DataFrame:
+    try:
+        # duplicate df
+        aug_df = repeat_df(df, n)
+        text = aug.augment(aug_df[text_col].tolist())
+        return pd.DataFrame({"label": aug_df[label_col], "text": text,})
+    except LookupError as e:
+        handle_lookups(e)
+        return synonym_augment(df, text_col=text_col, label_col=label_col, n=n)
+
---
+++ language-assignments/language-a4/tests/test_augment.py
@@ -0,0 +1,16 @@
+import src.augment as aug
+import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def test_synonym_augment():
+    df = pd.DataFrame({"label": [0, 1], "text": ["I love you", "I hate you"],})
+    aug_df = aug.synonym_augment(df, "text", "label", n=3)
+    logging.info(aug_df)
+    assert type(aug_df) == pd.DataFrame
+    assert aug_df.loc[0, "label"] == 0
+    assert aug_df.columns.tolist() == ["label", "text"]
+    assert aug_df.shape[0] == 6
+
