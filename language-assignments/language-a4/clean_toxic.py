9b715288f083a6b3306eabd725ec4ef1f4d83e52
Rysias <jhvithamar@gmail.com>
2022-05-29 11:33:47 +0200

    run A4 for the last time

--- language-assignments/language-a4/clean_toxic.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-if __name__ == "__main__":
-    rawdf = pd.read_csv("input/train.csv")
-    df = rawdf.loc[rawdf["toxic"] == 1, ["toxic", "comment_text"]]
-    df.columns = ["label", "text"]
-    df.to_csv("input/toxic_train.csv", index=False)
--- language-assignments/language-a4/run_project.sh
+++ language-assignments/language-a4/run_project.sh
@@ -1,10 +1,13 @@
 # Script for running everything :))
 echo "installing dependencies"
 bash setup.sh
-echo "Running the scripts..."
+echo "## Running the scripts... ##"
+echo "augmenting data"
+pipenv run python augment_data.py
 echo "Run logistic regression..."
-pipenv run python logistic_regression.py --dataset "../../../CDS-LANG/toxic/VideoCommentsThreatCorpus.csv"
+pipenv run python logistic_regression.py --dataset "input/VideoCommentsThreatCorpus.csv"
 echo "Done! Running neural network..."
-pipenv run python dnn_text.py --dataset "input/VideoCommentsThreatCorpus.csv" --epochs 5
+pipenv run python dnn_text.py --epochs 20 --dropout 0.1
 echo "Done with everything!"
+cat output/dnn_text.txt
 
